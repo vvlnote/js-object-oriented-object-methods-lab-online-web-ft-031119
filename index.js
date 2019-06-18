e294f74c28f90ea0b4f0b6d9919f6e9acaf40dea
Joy Lin <vvlnote@hotmail.com>
2019-06-18 17:35:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function BoardMember(name, homeState, training) {
     return `I like to help people.`;
   };
   this.releasePressStatement = function() {
-    return `You will see great things from Scuber`;
+    return `You will see great things from Scuber.`;
   };
   this.sayHi = function() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
